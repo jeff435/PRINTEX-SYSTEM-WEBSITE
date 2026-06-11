967382802a61b48be5d9701f3208cc961939c11b
Printex Admin <admin@printex.com>
2026-06-11 14:25:27 +0300

    feat: make printexengineers@gmail.com the main admin in SQLite database and scripts

--- app/applet/check_admin.js
+++ app/applet/check_admin.js
@@ -1,4 +1,4 @@
 import BetterSqlite3 from 'better-sqlite3';
 const db = new BetterSqlite3('printex.db');
-const user = db.prepare("SELECT * FROM users WHERE email = ?").get('admin');
+const user = db.prepare("SELECT * FROM users WHERE email = ?").get('printexengineers@gmail.com');
 console.log('Admin user:', user);
--- check_admin.js
+++ check_admin.js
@@ -1,5 +1,5 @@
 
 const Database = require('better-sqlite3');
 const db = new Database('printex.db');
-const user = db.prepare("SELECT * FROM users WHERE email = ?").get('admin');
+const user = db.prepare("SELECT * FROM users WHERE email = ?").get('printexengineers@gmail.com');
 console.log('Admin user:', user);
